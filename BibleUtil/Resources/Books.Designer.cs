85de625c1b55cc584ba9b4cd16d1491528ebbf62
Scott W. Vincent <scott@swvincent.com>
2020-01-15 20:04:54 -0500

    Go back to .NET Standard 1.3

--- BibleUtil/Resources/Books.Designer.cs
+++ BibleUtil/Resources/Books.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace BibleUtil.Resources {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace BibleUtil.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BibleUtil.Resources.Books", typeof(Books).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BibleUtil.Resources.Books", typeof(Books).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
